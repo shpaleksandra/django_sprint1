730b76417e2ad27d8db9c56ba8ddb17a01e8972e
Aleksandra Shapovalova <shapovalova.ao65@gmail.com>
2024-05-04 23:25:48 +0300

    shapovalova_ver1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -43,15 +43,18 @@ posts = [
     },
 ]
 
+
 def index(request):
     template_name = 'blog/index.html'
     context = {'posts': reversed(posts)}
     return render(request, template_name, context)
 
+
 def post_detail(request, id):
     template_name = 'blog/detail.html'
-    context = {'post': posts[id]}    
-    return render(request, template_name, context) 
+    context = {'post': posts[id]}
+    return render(request, template_name, context)
+
 
 def category_posts(request, category_slug):
     template_name = 'blog/category.html'
